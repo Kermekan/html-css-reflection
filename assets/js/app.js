290c53f8a9c321c567166b6fd1dcbfd124c3c3e4
Kermekan <odeemijr@gmail.com>
2022-02-01 16:40:55 +0000

    Added cookie popup and got it to work but it comes back on reload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,11 @@
-/* ------------------------------- Menu toggle ------------------------------ */
-const body = document.querySelector("body")
-const fullPage = document.querySelector(".full-page")
-const pageContainer = document.querySelector(".page-container")
 
-const sidebarContainer = document.querySelector(".sidebar-container")
-const overlay = document.querySelector(".overlay")
-const menuBtn = document.querySelector("#menu-btn");
-
-menuOpen = false;
-
-// Open menu
-menuBtn.addEventListener("click", () => {
-    if(!menuOpen) {
-        // add is-active class
-        menuBtn.classList.add("is-active");
-        sidebarContainer.classList.add("is-active");
-        pageContainer.classList.add("is-active");
-        fullPage.classList.add("is-active");
-        menuOpen = true;
-    }
-});
-
-// Close menu
-overlay.addEventListener("click", () => {
-    if(menuOpen == true){
-        menuBtn.classList.remove("is-active");
-        sidebarContainer.classList.remove("is-active");
-        pageContainer.classList.remove("is-active");
-        fullPage.classList.remove("is-active");
-        menuOpen = false;
-    }
-})
 
 /* ----------------------------- Image Carousel ----------------------------- */
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
         loop: true,
+        // autoWidth: true,
         autoplay: true,
         autoplaySpeed: 300,
         autoplayTimeout: 4000,
@@ -46,14 +15,47 @@ $(document).ready(function(){
 });
 
 /* ------------------------------ Sticky Header ----------------------------- */
-const header = document.getElementById("header");
+// const header = document.getElementById("header");
 // console.log(header);
 
 // let lastScroll = window.scrollY;
 
+/* ----------------------------- Cookie Consent ----------------------------- */
+const cookieStorage = {
+    getItem: (item) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+        return cookies[item];
+    },
+    setItem: (item, value) => {
+        document.cookie = `${item}=${value};`
+    }
+}
 
+const storageType = cookieStorage;
+const consentPropertyName = 'netmatters_cookie-consent';
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
 
+window.onload = () => {
+    const consentPopup = document.getElementById('cookie-consent');
+    const acceptBtn = document.getElementById('accept');
 
+    const acceptFn = event => {
+        saveToStorage(storageType);
+        consentPopup.classList.add('hidden');
+    }
+    acceptBtn.addEventListener('click', acceptFn);
+
+    if (shouldShowPopup(storageType.value == false)) {
+        setTimeout(() => {
+            consentPopup.classList.remove('hidden');
+        }, 2000);
+    }
+
+};
 
 
 
