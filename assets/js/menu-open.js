9adff44c519f697f36733d3a0fd83ae5770b6ee8
Kermekan <odeemijr@gmail.com>
2022-02-14 16:10:58 +0000

    no message

--- assets/js/menu-open.js
+++ assets/js/menu-open.js
@@ -5,24 +5,12 @@ const pageContainer = document.querySelector(".page-container")
 
 const sidebarContainer = document.querySelector(".sidebar-container")
 const overlay = document.querySelector(".overlay")
-const menuBtn = document.querySelector(".fixed-header #menu-btn");
-const menuBtn2 = document.querySelector(".sticky-header #menu-btn");
+const menuBtn1 = document.querySelector(".sticky-header .menu-btn");
+const menuBtn2 = document.querySelector(".fixed-header .menu-btn");
 
 menuOpen = false;
 
 // menuBtn1 open
-menuBtn.addEventListener("click", () => {
-    if(!menuOpen) {
-        // add is-active class
-        menuBtn.classList.add("is-active");
-        sidebarContainer.classList.add("is-active");
-        pageContainer.classList.add("is-active");
-        fullPage.classList.add("is-active");
-        menuOpen = true;
-    }
-});
-
-// menuBtn2 open
 menuBtn2.addEventListener("click", () => {
     if(!menuOpen) {
         // add is-active class
@@ -34,11 +22,23 @@ menuBtn2.addEventListener("click", () => {
     }
 });
 
+// menuBtn1 open
+menuBtn1.addEventListener("click", () => {
+    if(!menuOpen) {
+        // add is-active class
+        menuBtn1.classList.add("is-active");
+        sidebarContainer.classList.add("is-active");
+        pageContainer.classList.add("is-active");
+        fullPage.classList.add("is-active");
+        menuOpen = true;
+    }
+});
+
 // Close menu
 overlay.addEventListener("click", () => {
     if(menuOpen == true){
-        menuBtn.classList.remove("is-active");
         menuBtn2.classList.remove("is-active");
+        menuBtn1.classList.remove("is-active");
         sidebarContainer.classList.remove("is-active");
         pageContainer.classList.remove("is-active");
         fullPage.classList.remove("is-active");
