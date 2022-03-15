f65d8375faeb1fce6c39585d1bea46a9f7138382
Kermekan <odeemijr@gmail.com>
2022-03-15 09:59:07 +0000

    added gulp with browserSync

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// BrowserSync
+gulp.task('browserSync', function(){
+    browserSync.init({
+        proxy: 'http://localhost/my-projects/html-css-reflection',
+        browser: ['firefox', 'chrome'],
+        files: [
+            "css/*.css", "*.php", "inc/*.php", "js/*.js"
+        ]
+    });
+});
