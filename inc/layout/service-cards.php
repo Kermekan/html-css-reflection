332579041fdadfd68d016c0929ad4700a4e9d723
Kermekan <odeemijr@gmail.com>
2022-03-21 16:48:37 +0000

    added services php

---
+++ inc/layout/service-cards.php
@@ -0,0 +1,112 @@
+<!-- Cards -->
+<div class="service-cards">
+        <div class="inner service-cards-inner flex">
+            <!-- -------------------------- Individual Cards --------------------------- -->
+            <!--========== Bespoke ==========-->
+            <div class="card-container card card-xs software-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container">
+                        <span class="icon icon-apps"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">Bespoke Software</h4>
+                    <p class="description">
+                        Tailored software solutions to improve business productivity and online profits.
+                        Our expert team will ensure a software solution.
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+            <!--========== IT Support ==========-->
+            <div class="card-container card card-xs it-support-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container ">
+                        <span class="icon icon-display"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">IT Support</h4>
+                    <p class="description">
+                        Remotely managed IT services that is catered to your businesses needs, adds value and reduces costs.
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+            <!--========== Digital ==========-->
+            <div class="card-container card card-xs digital-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container ">
+                        <span class="icon icon-bars"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">Digital Marketing</h4>
+                    <p class="description">
+                        Driving brand awareness and ROI through creative digital marketing campaigns. We review and monitor online performances.
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+            <!--========== Telecoms ==========-->
+            <div class="card-container card card-sm telecoms-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container ">
+                        <span class="icon icon-phone_in_talk"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">Telecoms Services</h4>
+                    <p class="description">
+                        Stay connected with bespoke telecoms solutions when you need it most.
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+            <!--========== Web ==========-->
+            <div class="card-container card card-sm web-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container ">
+                        <span class="icon icon-code"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">Web Design</h4>
+                    <p class="description">
+                        User-centric design for businesses looking to make a lasting first impression.
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+            <!--========== Security ==========-->
+            <div class="card-container card card-sm security-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container ">
+                        <span class="icon icon-security"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">Cyber Security</h4>
+                    <p class="description">
+                        Ensuring your online business stays secure 24/7, 365 days of the year.
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+            <!--========== Training ==========-->
+            <div class="card-container card card-sm training-card">
+                <a href="#" class="card-inner flex">
+                    <span class="icon-container ">
+                        <span class="icon icon-graduation-cap"></span>
+                    </span>
+                    <!-- heading -->
+                    <h4 class="heading ">Developer Training</h4>
+                    <p class="description">
+                        Have you considered a career in web development but you aren't sure where to start?
+                    </p>
+                    <!-- Button -->
+                    <span class="btn services-btn text-uppercase">Read More</span>
+                </a>
+            </div>
+        </div><!-- .services-cards-inner -->
+    </div> <!-- .service-cards -->
