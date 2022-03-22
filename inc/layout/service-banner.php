d595dd2f5d273317e951bb2a860d9c83f3b69ea8
Kermekan <odeemijr@gmail.com>
2022-03-22 16:41:40 +0000

    miscellanaeous reformatting code

--- inc/layout/service-banner.php
+++ inc/layout/service-banner.php
@@ -1,143 +1,144 @@
 <!-- Banner -->
+
 <div class="banner flex">
-        <div class="slide-container owl-carousel">
-            <div class="slide flex web-slide">
-                <!-- Image -->
-                <img src="assets/images/banner-images/banner-1.jpg" alt="Web Design">
-                <!-- content -->
-                <div class="content inner flex">
-                    <div class="content--info flex">
-                        <div class="text">
-                            <h1>Web Design</h1>
-                            <p>
-                                For businesses looking to make a strong <br />
-                                and effective first impression.
-                            </p>
-                        </div>
-                        <!-- Button  -->
-                        <div class="btn banner-btn web-btn">
-                            <a href="#" class="text-center flex text-uppercase">
-                                Find Out More
-                                <span class="icon-arrow-thin-right"></span>
-                            </a>
-                        </div>
+    <div class="slide-container owl-carousel">
+        <div class="slide flex web-slide">
+            <!-- Image -->
+            <img src="assets/images/banner-images/banner-1.jpg" alt="Web Design">
+            <!-- content -->
+            <div class="content inner flex">
+                <div class="content--info flex">
+                    <div class="text">
+                        <h1>Web Design</h1>
+                        <p>
+                            For businesses looking to make a strong <br />
+                            and effective first impression.
+                        </p>
                     </div>
-                </div><!-- .content -->
-            </div>
-            <div class="slide flex it-support-slide">
-                <!-- Image -->
-                <img src="assets/images/banner-images/banner-2.jpg" alt="IT Support">
-                <!-- content -->
-                <div class="content inner flex">
-                    <div class="content--info flex">
-                        <div class="text">
-                            <h1>it support</h1>
-                            <p>
-                                Fast and cost-effective IT support <br />
-                                services for your business
-                            </p>
-                        </div>
-                        <!-- Button  -->
-                        <div class="btn banner-btn it-support-btn">
-                            <a href="#" class="text-center flex text-uppercase">
-                                Find Out More
-                                <span class="icon-arrow-thin-right"></span>
-                            </a>
-                        </div>
+                    <!-- Button  -->
+                    <div class="btn banner-btn web-btn">
+                        <a href="#" class="text-center flex text-uppercase">
+                            Find Out More
+                            <span class="icon-arrow-thin-right"></span>
+                        </a>
                     </div>
-                </div><!-- .content -->
-            </div>
-            <div class="slide flex telecoms-slide">
-                <!-- Image -->
-                <img src="assets/images/banner-images/banner-3.jpg" alt="Telecoms Services">
-                <!-- content -->
-                <div class="content inner flex">
-                    <div class="content--info flex">
-                        <div class="text">
-                            <h1>telecoms services</h1>
-                            <p>
-                                A new approach to connectivity, see how <br />
-                                we can help your business.
-                            </p>
-                        </div>
-                        <!-- Button  -->
-                        <div class="btn banner-btn telecoms-btn">
-                            <a href="#" class="text-center flex text-uppercase">
-                                Find Out More
-                                <span class="icon-arrow-thin-right"></span>
-                            </a>
-                        </div>
+                </div>
+            </div><!-- .content -->
+        </div>
+        <div class="slide flex it-support-slide">
+            <!-- Image -->
+            <img src="assets/images/banner-images/banner-2.jpg" alt="IT Support">
+            <!-- content -->
+            <div class="content inner flex">
+                <div class="content--info flex">
+                    <div class="text">
+                        <h1>it support</h1>
+                        <p>
+                            Fast and cost-effective IT support <br />
+                            services for your business
+                        </p>
                     </div>
-                </div><!-- .content -->
-            </div>
-            <div class="slide flex software-slide">
-                <!-- Image -->
-                <img src="assets/images/banner-images/banner-4.jpg" alt="Bespoke Software">
-                <!-- content -->
-                <div class="content inner flex">
-                    <div class="content--info flex">
-                        <div class="text">
-                            <h1>bespoke software</h1>
-                            <p>
-                                Bring your business together <br />
-                                with solutions that grow with you.
-                            </p>
-                        </div>
-                        <!-- Button  -->
-                        <div class="btn banner-btn software-btn">
-                            <a href="#" class="text-center flex text-uppercase">
-                                Find Out More
-                                <span class="icon-arrow-thin-right"></span>
-                            </a>
-                        </div>
+                    <!-- Button  -->
+                    <div class="btn banner-btn it-support-btn">
+                        <a href="#" class="text-center flex text-uppercase">
+                            Find Out More
+                            <span class="icon-arrow-thin-right"></span>
+                        </a>
                     </div>
-                </div><!-- .content -->
-            </div>
-            <div class="slide flex digital-slide">
-                <!-- Image -->
-                <img src="assets/images/banner-images/banner-5.jpg" alt="Digital Marketing">
-                <!-- content -->
-                <div class="content inner flex">
-                    <div class="content--info flex">
-                        <div class="text">
-                            <h1>digital marketing</h1>
-                            <p>
-                                Generating you new business through <br />
-                                results-driven marketing activities.
-                            </p>
-                        </div>
-                        <!-- Button  -->
-                        <div class="btn banner-btn digital-btn">
-                            <a href="#" class="text-center flex text-uppercase">
-                                See How We Can Help You
-                                <span class="icon-arrow-thin-right"></span>
-                            </a>
-                        </div>
+                </div>
+            </div><!-- .content -->
+        </div>
+        <div class="slide flex telecoms-slide">
+            <!-- Image -->
+            <img src="assets/images/banner-images/banner-3.jpg" alt="Telecoms Services">
+            <!-- content -->
+            <div class="content inner flex">
+                <div class="content--info flex">
+                    <div class="text">
+                        <h1>telecoms services</h1>
+                        <p>
+                            A new approach to connectivity, see how <br />
+                            we can help your business.
+                        </p>
                     </div>
-                </div><!-- .content -->
-            </div>
-            <div class="slide flex security-slide">
-                <!-- Image -->
-                <img src="assets/images/banner-images/banner-6.jpg" alt="Cyber Security">
-                <!-- content -->
-                <div class="content inner flex">
-                    <div class="content--info flex">
-                        <div class="text">
-                            <h1>cyber security</h1>
-                            <p>
-                                Keeping businesses and their customers <br />
-                                sensitive information protected.
-                            </p>
-                        </div>
-                        <!-- Button  -->
-                        <div class="btn banner-btn security-btn">
-                            <a href="#" class="text-center flex text-uppercase">
-                                Find Out More
-                                <span class="icon-arrow-thin-right"></span>
-                            </a>
-                        </div>
+                    <!-- Button  -->
+                    <div class="btn banner-btn telecoms-btn">
+                        <a href="#" class="text-center flex text-uppercase">
+                            Find Out More
+                            <span class="icon-arrow-thin-right"></span>
+                        </a>
                     </div>
-                </div><!-- .content -->
-            </div>
-        </div><!-- .slide-container -->
-    </div><!-- .banner -->
+                </div>
+            </div><!-- .content -->
+        </div>
+        <div class="slide flex software-slide">
+            <!-- Image -->
+            <img src="assets/images/banner-images/banner-4.jpg" alt="Bespoke Software">
+            <!-- content -->
+            <div class="content inner flex">
+                <div class="content--info flex">
+                    <div class="text">
+                        <h1>bespoke software</h1>
+                        <p>
+                            Bring your business together <br />
+                            with solutions that grow with you.
+                        </p>
+                    </div>
+                    <!-- Button  -->
+                    <div class="btn banner-btn software-btn">
+                        <a href="#" class="text-center flex text-uppercase">
+                            Find Out More
+                            <span class="icon-arrow-thin-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div><!-- .content -->
+        </div>
+        <div class="slide flex digital-slide">
+            <!-- Image -->
+            <img src="assets/images/banner-images/banner-5.jpg" alt="Digital Marketing">
+            <!-- content -->
+            <div class="content inner flex">
+                <div class="content--info flex">
+                    <div class="text">
+                        <h1>digital marketing</h1>
+                        <p>
+                            Generating you new business through <br />
+                            results-driven marketing activities.
+                        </p>
+                    </div>
+                    <!-- Button  -->
+                    <div class="btn banner-btn digital-btn">
+                        <a href="#" class="text-center flex text-uppercase">
+                            See How We Can Help You
+                            <span class="icon-arrow-thin-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div><!-- .content -->
+        </div>
+        <div class="slide flex security-slide">
+            <!-- Image -->
+            <img src="assets/images/banner-images/banner-6.jpg" alt="Cyber Security">
+            <!-- content -->
+            <div class="content inner flex">
+                <div class="content--info flex">
+                    <div class="text">
+                        <h1>cyber security</h1>
+                        <p>
+                            Keeping businesses and their customers <br />
+                            sensitive information protected.
+                        </p>
+                    </div>
+                    <!-- Button  -->
+                    <div class="btn banner-btn security-btn">
+                        <a href="#" class="text-center flex text-uppercase">
+                            Find Out More
+                            <span class="icon-arrow-thin-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div><!-- .content -->
+        </div>
+    </div><!-- .slide-container -->
+</div><!-- .banner -->
--- index.php
+++ index.php
@@ -7,15 +7,22 @@ include __DIR__ . './inc/layout/sidebar.php';
 ?>
 <!-- Opening Body Tag -->
 
-<!-- Header -->
 
 <div class="page-container">
+    <!-- Header -->
     <?php include __DIR__ . './inc/layout/header.php';?>
     <!-- Services -->
     <section id="services" class="services">
         <?php include __DIR__ . './inc/layout/service-banner.php';?>
         <?php include __DIR__ . './inc/layout/service-cards.php';?>
     </section>
+    
+    <?php include __DIR__ . './inc/layout/about.php';?>
+    <?php include __DIR__ . './inc/layout/news.php';?>
+    <?php include __DIR__ . './inc/layout/clients.php';?>
+    <?php include __DIR__ . './inc/layout/newsletter.php';?>
+    <!-- Footer -->
+    <?php include __DIR__ . './inc/layout/footer.php';?>
 </div>
 
 <!-- Ending body tag & JS links -->
